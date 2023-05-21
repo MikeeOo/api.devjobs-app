2ce073e39ce83be70c2cccacab87be194e5a5bc8
MikeeOo <michal.olszewskki@gmail.com>
2023-05-21 15:50:42 +0200

    basic express setup

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,10 @@
-console.log("Hello Node!");
+import express from "express";
+
+const server = express();
+const port = 8080;
+
+server.get(`/`, (req, res) => {
+    res.json(`Hello World!`);
+});
+
+server.listen(port, () => console.log(`Server listening on port ${port}`));
