a81f6065187d4acc1ad49c0f36eecae3e0ac04e3
MikeeOo <michal.olszewskki@gmail.com>
2023-06-14 20:43:36 +0200

    created generateToken utility

---
+++ src/utils/generateToken.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from "express";
+import jwt from "jsonwebtoken";
+
+const generateToken = (res: Response, userId: string): void => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+    });
+
+    res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development",
+        sameSite: "strict",
+        maxAge: 2 * 60 * 60 * 1000,
+    });
+};
+
+export default generateToken;
