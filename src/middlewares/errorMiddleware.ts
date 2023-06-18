f14de82c86dedd7b5b33c4c60d7875acbf4fc09c
MikeeOo <michal.olszewskki@gmail.com>
2023-06-18 18:59:31 +0200

    fixed ts errors in errorMiddleware

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -1,16 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 
+interface Error {
+    name: string;
+    message: string;
+    kind: string;
+    stack: string;
+}
+
 const notFound = (req: Request, res: Response, next: NextFunction) => {
-    const error = new Error(`Not Found - ${originalUrl}`);
+    const error = new Error(`Not Found - ${req.originalUrl}`);
     res.status(404);
     next(error);
 };
 
-const errorHandler = (err,, req: Request, res: Response, next: NextFunction) => {
+const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
     let message = err.message;
 
-    if (err.name === "CastError" && err.king === "ObjectId") {
+    if (err.name === "CastError" && err.kind === "ObjectId") {
         statusCode = 404;
         message = "Resource not found";
     }
