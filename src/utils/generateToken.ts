b3e3d1e67d12a650954aa5a9cfe552b5091dda8d
MikeeOo <michal.olszewskki@gmail.com>
2023-07-05 18:14:49 +0200

    refactored types in genereateToken utility

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,19 +1,17 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import jwt from "jsonwebtoken";
 
 const generateToken = (res: Response, userId: string): void => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "2h",
-    });
+	const token: string = jwt.sign({ userId }, process.env.JWT_SECRET, {
+		expiresIn: "2h",
+	});
 
-    res.cookie("jwt", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
-        maxAge: 2 * 60 * 60 * 1000,
-    });
+	res.cookie("jwt", token, {
+		httpOnly: true,
+		secure: process.env.NODE_ENV !== "development",
+		sameSite: "strict",
+		maxAge: 2 * 60 * 60 * 1000,
+	});
 };
 
 export default generateToken;
-
-
